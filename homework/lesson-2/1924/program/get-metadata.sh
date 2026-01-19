1a72b08d9e46d3d2fbb870d7f44b62e29f9d315e
Soldier-Hacker <2564563208@qq.com>
2026-01-19 20:18:31 +0800

    1924

---
+++ homework/lesson-2/1924/program/get-metadata.sh
@@ -0,0 +1,2 @@
+npx papi add devnet -w ws://localhost:9944
+npx papi add hub -w wss://asset-hub-paseo-rpc.n.dwellir.com
---
+++ homework/lesson-2/1924/program/src/precompile.ts
@@ -0,0 +1,20 @@
+import { ethers } from "ethers"
+import { getApi, getProvider } from "./utils";
+
+const identity_precompile = "0x0000000000000000000000000000000000000004"
+const hash_precompile = "0x0000000000000000000000000000000000000002"
+
+export async function callWithoutSelector(provider: ethers.JsonRpcProvider) {
+    const result = await provider.call({
+        to: identity_precompile,
+        data: "0x12345678"
+    });
+
+    console.log("Result:", result);
+}
+async function main() {
+    const provider = getProvider(true);
+    await callWithoutSelector(provider);
+}
+
+main();
